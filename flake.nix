5db9b7c4853a38f65b08a24a935e7dfce9222852
Tyler Mayoff <tyler@tylermayoff.com>
2024-05-14 18:16:26 -0400

    Organization

--- flake.nix
+++ flake.nix
@@ -13,54 +13,55 @@
           inherit system;
         };
       in rec {
-        mpc = pkgs.stdenv.mkDerivation {};
-        opendds = pkgs.stdenv.mkDerivation {
-          name = "OpenDDS";
-          srcs = [
-            (pkgs.fetchFromGitHub {
-              name = "OpenDDS";
-              owner = "OpenDDS";
-              repo = "OpenDDS";
-              rev = "DDS-3.28.1";
-              hash = "sha256-aa+4RLtu6gY6QrV2t9MzLiw9bo0VimGBt0aVeDS9GeU=";
-            })
-            (pkgs.fetchFromGitHub {
-              name = "ACE_TAO";
-              owner = "DOCGroup";
-              repo = "ACE_TAO";
-              rev = "ACE+TAO-7_1_4";
-              hash = "sha256-ICTChzwFkQMymT60T5DiyKLSd7PcP8TXcU6RfRJr5Uw=";
-            })
-            (pkgs.fetchFromGitHub {
-              name = "MPC";
-              owner = "DOCGroup";
-              repo = "MPC";
-              rev = "master";
-              hash = "sha256-AEm4CI3GVa3eHOVFBTTMooVXYeW0qywZomQfTvJ/Lvs=";
-            })
-          ];
+        packages = {
+          default = pkgs.stdenv.mkDerivation {
+            name = "OpenDDS";
+            srcs = [
+              (pkgs.fetchFromGitHub {
+                name = "OpenDDS";
+                owner = "OpenDDS";
+                repo = "OpenDDS";
+                rev = "DDS-3.28.1";
+                hash = "sha256-aa+4RLtu6gY6QrV2t9MzLiw9bo0VimGBt0aVeDS9GeU=";
+              })
+              (pkgs.fetchFromGitHub {
+                name = "ACE_TAO";
+                owner = "DOCGroup";
+                repo = "ACE_TAO";
+                rev = "ACE+TAO-7_1_4";
+                hash = "sha256-ICTChzwFkQMymT60T5DiyKLSd7PcP8TXcU6RfRJr5Uw=";
+              })
+              (pkgs.fetchFromGitHub {
+                name = "MPC";
+                owner = "DOCGroup";
+                repo = "MPC";
+                rev = "master";
+                hash = "sha256-AEm4CI3GVa3eHOVFBTTMooVXYeW0qywZomQfTvJ/Lvs=";
+              })
+            ];
 
-          sourceRoot = ".";
+            sourceRoot = ".";
 
-          nativeBuildInputs = with pkgs; [
-            cmake
-            perl
-          ];
+            nativeBuildInputs = with pkgs; [
+              cmake
+              perl
+            ];
 
-          configurePhase = ''
-            cmake -SOpenDDS -BOpenDDS/build -DOPENDDS_ACE_TAO_SRC="$(pwd)/ACE_TAO" -DOPENDDS_MPC="$(pwd)/MPC" -DOPENDDS_RAPIDJSON="" -DCMAKE_INSTALL_PREFIX=$prefix
-          '';
+            configurePhase = ''
+              cmake -SOpenDDS -BOpenDDS/build -DOPENDDS_ACE_TAO_SRC="$(pwd)/ACE_TAO" -DOPENDDS_MPC="$(pwd)/MPC" -DOPENDDS_RAPIDJSON="" -DCMAKE_INSTALL_PREFIX=$prefix
+            '';
 
-          buildPhase = ''
-            cmake --build OpenDDS/build -j $NIX_BUILD_CORES
-          '';
+            buildPhase = ''
+              cmake --build OpenDDS/build -j $NIX_BUILD_CORES
+            '';
 
-          installPhase = ''
-            cmake --install OpenDDS/build
-          '';
+            installPhase = ''
+              cmake --install OpenDDS/build
+            '';
+          };
         };
 
-        defaultPackage = opendds;
+        defaultPackage = packages.default;
 
         devShell = pkgs.mkShell.override {stdenv = pkgs.clangStdenv;} {
           nativeBuildInputs = with pkgs; [
