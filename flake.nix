ccd7bc5aefe3d7b5470c61f8e50c556e59d51aef
Tyler Mayoff <tyler@tylermayoff.com>
2024-05-14 20:20:43 -0400

    Moved package to it's own file

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake utils demo";
+  description = "Packaging OpenDDS with nix";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = {
@@ -13,52 +13,7 @@
           inherit system;
         };
       in {
-        packages.opendds = pkgs.stdenv.mkDerivation {
-          name = "OpenDDS";
-          srcs = [
-            (pkgs.fetchFromGitHub {
-              name = "OpenDDS";
-              owner = "OpenDDS";
-              repo = "OpenDDS";
-              rev = "DDS-3.28.1";
-              hash = "sha256-aa+4RLtu6gY6QrV2t9MzLiw9bo0VimGBt0aVeDS9GeU=";
-            })
-            (pkgs.fetchFromGitHub {
-              name = "ACE_TAO";
-              owner = "DOCGroup";
-              repo = "ACE_TAO";
-              rev = "ACE+TAO-7_1_4";
-              hash = "sha256-ICTChzwFkQMymT60T5DiyKLSd7PcP8TXcU6RfRJr5Uw=";
-            })
-            (pkgs.fetchFromGitHub {
-              name = "MPC";
-              owner = "DOCGroup";
-              repo = "MPC";
-              rev = "master";
-              hash = "sha256-AEm4CI3GVa3eHOVFBTTMooVXYeW0qywZomQfTvJ/Lvs=";
-            })
-          ];
-
-          sourceRoot = ".";
-
-          nativeBuildInputs = with pkgs; [
-            cmake
-            perl
-          ];
-
-          configurePhase = ''
-            cmake -SOpenDDS -BOpenDDS/build -DOPENDDS_ACE_TAO_SRC="$(pwd)/ACE_TAO" -DOPENDDS_MPC="$(pwd)/MPC" -DOPENDDS_RAPIDJSON="" -DCMAKE_INSTALL_PREFIX=$prefix
-          '';
-
-          buildPhase = ''
-            cmake --build OpenDDS/build -j $NIX_BUILD_CORES
-          '';
-
-          installPhase = ''
-            cmake --install OpenDDS/build
-          '';
-        };
-
+        packages.opendds = pkgs.callPackage ./package.nix {};
         packages.default = self.packages.${system}.opendds;
       }
     );
